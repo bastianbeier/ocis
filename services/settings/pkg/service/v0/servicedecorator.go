d205228f4732dc5abf19c2bc25d726a310385c18
Roman Perekhod <rperekhod@owncloud.com>
2024-12-16 11:56:11 +0100

    Ensure the default values for profile settings

--- services/settings/pkg/service/v0/servicedecorator.go
+++ services/settings/pkg/service/v0/servicedecorator.go
@@ -2,6 +2,7 @@ package svc
 
 import (
 	"context"
+	"net/http"
 	"strings"
 
 	settingsmsg "github.com/owncloud/ocis/v2/protogen/gen/ocis/messages/settings/v0"
@@ -9,6 +10,7 @@ import (
 	"github.com/owncloud/ocis/v2/services/settings/pkg/config"
 	"github.com/owncloud/ocis/v2/services/settings/pkg/settings"
 	"github.com/owncloud/ocis/v2/services/settings/pkg/store/defaults"
+	merrors "go-micro.dev/v4/errors"
 )
 
 var _defaultLanguage = "en"
@@ -31,9 +33,13 @@ type defaultLanguageDecorator struct {
 func (s *defaultLanguageDecorator) GetValueByUniqueIdentifiers(ctx context.Context, req *settingssvc.GetValueByUniqueIdentifiersRequest, res *settingssvc.GetValueResponse) error {
 	err := s.ServiceHandler.GetValueByUniqueIdentifiers(ctx, req, res)
 	if err != nil {
-		if strings.Contains(strings.ToLower(err.Error()), "not found") && req.GetSettingId() == defaults.SettingUUIDProfileLanguage && res.GetValue() == nil {
-			res.Value = s.withDefaultLanguageSetting(req.AccountUuid)
-			return nil
+		if strings.Contains(strings.ToLower(err.Error()), "not found") && res.GetValue() == nil {
+			defaultValueList := getDefaultValueList()
+			// Ensure the default values for profile settings
+			if _, ok := defaultValueList[req.GetSettingId()]; ok {
+				res.Value = s.withDefaultProfileValue(ctx, req.AccountUuid, req.GetSettingId())
+				return nil
+			}
 		}
 		return err
 	}
@@ -46,13 +52,23 @@ func (s *defaultLanguageDecorator) ListValues(ctx context.Context, req *settings
 	if err != nil {
 		return err
 	}
+	defaultValueList := getDefaultValueList()
 	for _, v := range res.Values {
-		if v.GetValue().GetSettingId() == defaults.SettingUUIDProfileLanguage {
-			return nil
+		if _, ok := defaultValueList[v.GetValue().GetSettingId()]; ok {
+			delete(defaultValueList, v.GetValue().GetSettingId())
 		}
 	}
 
-	res.Values = append(res.Values, s.withDefaultLanguageSetting(req.AccountUuid))
+	// Ensure the default values for profile settings
+	defaultValueList = s.withDefaultProfileValueList(ctx, req.AccountUuid, defaultValueList)
+	if len(defaultValueList) > 0 {
+		for _, v := range defaultValueList {
+			if v != nil {
+				res.Values = append(res.Values, v)
+			}
+		}
+	}
+
 	return nil
 }
 
@@ -80,5 +96,97 @@ func (s *defaultLanguageDecorator) withDefaultLanguageSetting(accountUUID string
 				}},
 			},
 		},
+	}
+}
+
+func (s *defaultLanguageDecorator) withDefaultProfileValue(ctx context.Context, accountUUID string, settingId string) *settingsmsg.ValueWithIdentifier {
+	if settingId == defaults.SettingUUIDProfileLanguage {
+		return s.withDefaultLanguageSetting(accountUUID)
+	}
+	res := s.withDefaultProfileValueList(ctx, accountUUID, map[string]*settingsmsg.ValueWithIdentifier{settingId: nil})
+	if v, ok := res[settingId]; ok {
+		return v
+	}
+	return nil
+}
+
+func (s *defaultLanguageDecorator) withDefaultProfileValueList(ctx context.Context,
+	accountUUID string, requested map[string]*settingsmsg.ValueWithIdentifier) map[string]*settingsmsg.ValueWithIdentifier {
+	req := &settingssvc.GetBundleRequest{BundleId: defaults.BundleUUIDProfile}
+	resp := &settingssvc.GetBundleResponse{}
+	if err := s.GetBundle(ctx, req, resp); err != nil {
+		if merr, ok := merrors.As(err); ok && merr.Code != http.StatusNotFound {
+			return requested
+		}
+	}
+
+	for _, setting := range resp.GetBundle().GetSettings() {
+		if v, ok := requested[setting.GetId()]; ok && v == nil {
+			if setting.GetId() == defaults.SettingUUIDProfileLanguage {
+				requested[setting.GetId()] = s.withDefaultLanguageSetting(accountUUID)
+				continue
+			}
+
+			newVal := &settingsmsg.ValueWithIdentifier{
+				Identifier: &settingsmsg.Identifier{
+					Extension: resp.GetBundle().GetExtension(),
+					Bundle:    resp.GetBundle().GetName(),
+					Setting:   setting.GetName(),
+				},
+				Value: &settingsmsg.Value{
+					BundleId:    resp.GetBundle().GetId(),
+					SettingId:   setting.GetId(),
+					AccountUuid: accountUUID,
+					Resource:    setting.GetResource(),
+				},
+			}
+
+			switch val := setting.GetValue().(type) {
+			case *settingsmsg.Setting_MultiChoiceCollectionValue:
+				newVal.Value.Value = multiChoiceCollectionToValue(val.MultiChoiceCollectionValue)
+				requested[setting.GetId()] = newVal
+			}
+		}
+	}
+
+	return requested
+}
+
+func multiChoiceCollectionToValue(collection *settingsmsg.MultiChoiceCollection) *settingsmsg.Value_CollectionValue {
+	values := make([]*settingsmsg.CollectionOption, 0, len(collection.GetOptions()))
+	for _, option := range collection.GetOptions() {
+		switch o := option.GetValue().GetOption().(type) {
+		case *settingsmsg.MultiChoiceCollectionOptionValue_BoolValue:
+			if o != nil {
+				values = append(values, &settingsmsg.CollectionOption{
+					Key: option.GetKey(),
+					Option: &settingsmsg.CollectionOption_BoolValue{
+						BoolValue: o.BoolValue.GetDefault(),
+					},
+				})
+			}
+		}
+	}
+
+	return &settingsmsg.Value_CollectionValue{
+		CollectionValue: &settingsmsg.CollectionValue{
+			Values: values,
+		},
+	}
+}
+
+func getDefaultValueList() map[string]*settingsmsg.ValueWithIdentifier {
+	return map[string]*settingsmsg.ValueWithIdentifier{
+		defaults.SettingUUIDProfileLanguage:                             nil,
+		defaults.SettingUUIDProfileEventShareCreated:                    nil,
+		defaults.SettingUUIDProfileEventShareRemoved:                    nil,
+		defaults.SettingUUIDProfileEventShareExpired:                    nil,
+		defaults.SettingUUIDProfileEventSpaceShared:                     nil,
+		defaults.SettingUUIDProfileEventSpaceUnshared:                   nil,
+		defaults.SettingUUIDProfileEventSpaceMembershipExpired:          nil,
+		defaults.SettingUUIDProfileEventSpaceDisabled:                   nil,
+		defaults.SettingUUIDProfileEventSpaceDeleted:                    nil,
+		defaults.SettingUUIDProfileEventPostprocessingStepFinished:      nil,
+		defaults.SettingUUIDProfileEventScienceMeshInviteTokenGenerated: nil,
 	}
 }
